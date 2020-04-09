97dc79e7100c0bdf268b97b7729bee74a6cb5981
Jorma Laaksonen <jorma.laaksonen@aalto.fi>
2020-04-09 19:27:39 +0300

    Implemented sample picking from sluster centers.

--- cluster.py
+++ cluster.py
@@ -2,14 +2,19 @@
 
 import scipy.cluster as cluster
 import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(42)
 
 #method='single'
 method='complete'
 #method=''
 #method=''
 
-val=20
-criterion='maxclust'
+n         = 10000
+val       =   100
+max_m     =    35
+criterion = 'maxclust'
 
 dat = []
 lab = []
@@ -22,31 +27,51 @@ with open('features.dat') as f:
         dat.append(ll)
         lab.append(lx.strip())
 
-n = len(dat)
-#n = 100
+# s = [ i for i in range(n) ]
+s = np.random.permutation(len(dat))[:n]
+# print(s)
+# exit(0)
 
-s = [ i for i in range(n) ]
 sel = []
 for i in range(len(s)):
     sel.append(dat[s[i]])
 
 link = cluster.hierarchy.linkage(sel, method=method)
-print(link)
+#print(link)
 #plt.figure()
 #dn = cluster.hierarchy.dendrogram(link)
 #plt.show()
 
 fc = cluster.hierarchy.fcluster(link, val, criterion=criterion)
-print(fc)
+#print(fc)
 
 for i in range(val):
-    print(r'LATEX \clusterbegins{',i+1,'}', sep='') 
-    #print(i+1, end=' ')
+    x = []
+    y = []
     for j, k in enumerate(fc):
         if k==i+1:
-            print(r'LATEX \clusterentry{',lab[j],'}', sep='') 
-            #print(lab[j], end=' ')
-    #print()
+            x.append(sel[j])
+            y.append(lab[s[j]])
+
+    m = np.mean(x, axis=0)
+    #print(m)
+    #print(type(x-m), (x-m).shape)
+    d = np.linalg.norm(x-m, axis=1)
+    #print(d)
+    #print(y)
+    d = zip(range(len(d)), y, d)
+    e = sorted(d, key=lambda a: a[2])
+    #print(e)
+
+    eff_m = len(e)//2
+    if eff_m>max_m:
+        eff_m = max_m
+    
+    print(r'LATEX \clusterbegins{',i+1,' total ', len(x), '}', sep='') 
+    for j in range(eff_m):
+        print(r'LATEX \clusterentry{',e[j][1],'}', sep='') 
+        print(r'LABEL [', i+1, '] ', e[j][1], sep='') 
+
     print(r'LATEX \clusterends{',i+1,'}', sep='') 
 
     
